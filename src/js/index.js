d23d3f8e339b90b2173178def5912f08b11ce0fa
Sandro Greco <grecosandro@outlook.com>
2020-05-25 11:47:05 +0200

    new branch develop

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+import 'angular';
+import 'angular-aria';
+import 'angular-animate';
+import 'angular-material';
+import 'angular-messages';
+import '../css/style.scss';
+import 'angular-material/angular-material.css';
+import img1 from '../assets/icons/01.svg';
+import img2 from '../assets/icons/02.svg';
+import img3 from '../assets/icons/03.svg';
+import img4 from '../assets/icons/04.svg';
+import img5 from '../assets/icons/05.svg';
+import img6 from '../assets/icons/06.svg';
+
+// Url dell'API
+let urlApi = 'https://api.covid19api.com/summary';
+
+export const app = angular.module("covid19", ['ngMaterial', 'ngMessages']);
+
+app.controller("test", ($scope) => {
+    fetch(urlApi)
+        .then(response => response.json())
+        .then(data => {
+            $scope.date = data.Date;
+            $scope.global = data.Global;
+            $scope.countries = data.Countries;
+            $scope.$apply()
+        });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,106 @@
 const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+
+const PATHS = {
+    src: path.resolve(__dirname, 'src'),
+    dist: path.resolve(__dirname, 'dist'),
+    assets: ""
+};
+
+const PAGES = fs
+    .readdirSync(PATHS.src)
+    .filter(fileName => fileName.endsWith(".html"));
+
+console.log(PAGES);
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  target: 'web',
+    mode: 'none',
+    entry: {
+        app: './src/js/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'src/js/main.js'
+    },
+    target: 'web',
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: "vendors",
+                    test: /node_modules/,
+                    chunks: "all",
+                    enforce: true
+                }
+            }
+        }
+    },
+    module:{
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        // File loader
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'src/assets/icons/[name].[ext]'
+              },
+            },
+          ],
+        },
+        // CSS loader
+        {
+          test: /\.css$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+        // Sass loader
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ]
+    },
+    plugins: [
+        // new CopyWebpackPlugin([]),
+        ...PAGES.map(
+            page => new HtmlWebpackPlugin({
+                template: `${PATHS.src}/${page}`,
+                filename: `./${page}`,
+                minify: true
+            })
+        )
+    ]
 };
