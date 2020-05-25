271a88420d234aea990734797b6e36b62736c86b
Sandro Greco <grecosandro@outlook.com>
2020-05-25 12:07:17 +0200

    delete all from master

--- src/index.js
+++
@@ -1,7 +0,0 @@
-
-const app = angular.module("covid19", []);
-app.controller("test", function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-    console.log('ci sono');
-});
